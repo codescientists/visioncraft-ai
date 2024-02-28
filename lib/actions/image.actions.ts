3f105668798d015c2ca01d6e6633b42322004ac3
codescientists <piyushpp2412@gmail.com>
2024-02-28 16:33:15 +0530

    Improved Design

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -108,7 +108,7 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       secure: true,
     })
 
-    let expression = 'folder=imaginify';
+    let expression = 'folder=visioncraft';
 
     if (searchQuery) {
       expression += ` AND ${searchQuery}`
